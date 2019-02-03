902e39b96667933007c26e194f7b2f28b261b7fa
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-02-03 10:55:45 +0530

    first_gui by tkinter with python3

---
+++ GUI_first.py
@@ -0,0 +1,11 @@
+#import all classes from tkinter
+from tkinter import *
+
+hk = Tk()
+#create label 
+mylabel = Label(hk,text="my 1st demo")
+#stabe windows 
+mylabel.pack()
+#infinite loop
+hk.mainloop()
+
