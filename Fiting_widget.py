4b67edb0bb500b4d5626feb6ad3512ea5daefe13
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-02-22 12:49:16 +0530

    fiting widget in frame in tkinter gui

---
+++ Fiting_widget.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root = Tk()
+
+Bt1=Button(root,text="Button",bg='red')
+Bt1.pack(fill=X)
+Bt2=Button(root,text="Button",bg="green")
+Bt2.pack(side=LEFT,fill=Y)
+root.mainloop()
