1c84c40e5a21de9c4af83dc6a630ed0c6716e360
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-03-01 16:35:33 +0530

    Messagebox_askquestion in tkinter

---
+++ Messagebox_askquestion.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+
+def call_me():
+	answer=messagebox.askquestion("exit","Do you sure to exit")
+	print(answer)
+
+root=Tk()
+b1=Button(root,text="Exit",command=call_me)
+b1.pack()
+
+root.geometry("400x300+150+150")
+root.mainloop()
