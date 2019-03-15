decde934225ace7db9026d570fde737b05cdd658
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-03-15 21:54:30 +0530

    optimise code

--- 12_canvas_create_line.py
+++ 12_canvas_create_line.py
@@ -7,6 +7,8 @@ canvas=Canvas(root,width=250,height=250,bg='yellow')
 canvas.pack()
 #create red line from (250,0) to (0,100)
 red_line=canvas.create_line(250,0,0,100,fill='red')
+
+#create blue line from (0,100) to (250,250)
 blue_line=canvas.create_line(0,100,250,250,fill='blue')
 root.geometry("400x400+150+150")
 root.mainloop()
