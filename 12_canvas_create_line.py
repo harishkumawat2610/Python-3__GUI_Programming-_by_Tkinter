9232f5d3964832b135c08ca4d480bb615f1c153c
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-03-15 21:53:22 +0530

    commited code

--- 12_canvas_create_line.py
+++ 12_canvas_create_line.py
@@ -5,6 +5,7 @@ root=Tk()
 #create canvas with height 250 and width 250
 canvas=Canvas(root,width=250,height=250,bg='yellow')
 canvas.pack()
+#create red line from (250,0) to (0,100)
 red_line=canvas.create_line(250,0,0,100,fill='red')
 blue_line=canvas.create_line(0,100,250,250,fill='blue')
 root.geometry("400x400+150+150")
